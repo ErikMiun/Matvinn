7c843b4526df5d3036351bffdb4a0eb6524eb927
Erik <erbe2201@student.miun.se>
2023-04-18 20:06:19 +0200

    Knappar för att ta sig till skafferi och kamera

--- app/src/main/java/com/example/matvinn/MainActivity.java
+++ app/src/main/java/com/example/matvinn/MainActivity.java
@@ -9,18 +9,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_inventory;
+    Button btn_inventory, btn_camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_inventory = findViewById(R.id.btn_inventory);
+        btn_camera = findViewById(R.id.btn_camera);
 
         btn_inventory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Inventory.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Camera.class);
                 startActivity(intent);
             }
         });
