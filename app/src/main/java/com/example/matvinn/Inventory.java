938c6c1cb0b1e15d3d2357ee7e7e1a7b9a48da94
Erik <erbe2201@student.miun.se>
2023-04-18 18:19:53 +0200

    Påbörjad lista för "skafferi" och funktion för att lägga till objekt

--- app/src/main/java/com/example/matvinn/Inventory.java
+++ app/src/main/java/com/example/matvinn/Inventory.java
@@ -2,13 +2,27 @@ package com.example.matvinn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inventory extends AppCompatActivity {
 
+    Button btn_addItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        btn_addItem = findViewById(R.id.btn_addItem);
+
+        btn_addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Inventory.this, AddEditOne.class);
+                startActivity(intent);
+            }
+        });
     }
 }
