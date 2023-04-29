ee9bebce4635497e85ebf824232b254fbabe78a3
only74 <120048264+only74@users.noreply.github.com>
2023-04-29 16:04:44 +0200

    //Camera funktionalitet.

--- app/src/main/java/com/example/matvinn/Camera.java
+++ app/src/main/java/com/example/matvinn/Camera.java
@@ -1,15 +1,42 @@
 package com.example.matvinn;
 
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class Camera extends AppCompatActivity {
-
-
+    private ImageView imageView; private Button button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        imageView = findViewById(R.id.scannedCode);
+        button = findViewById(R.id.btnTake);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(photo);
     }
 }
+
